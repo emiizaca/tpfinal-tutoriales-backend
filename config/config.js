2b7615edcf22ea1d262409ae18547f1467bfd4e4
Carlos Emiliano Zacarias <emii.zacarias@gmail.com>
2020-07-06 02:56:35 -0300

    nuevo config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const {Pool} = require('pg');
 const isProduction = process.env.NODE_ENV === 'production'
 
@@ -6,8 +7,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl : isProduction
-  //ssl: {rejectUnauthorized: false} 
+  ssl : isProduction,
 })
 
 module.exports =  { pool };
